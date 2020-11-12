0e9503a5e27bea1316fb336ba788097086e675f2
Jacob Karcz <jacob.karcz@adswerve.com>
2020-11-12 15:06:24 -0800

    updated setup to specify last working mws version for inventory

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='singer-tap-amazon-mws',
       py_modules=['singer_tap_amazon_mws'],
       install_requires=[
           'tap-framework==0.0.4',
-          'mws',
+          'mws==0.7.4',
       ],
       entry_points='''
           [console_scripts]
